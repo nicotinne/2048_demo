30494874a5c2cc86aa184969957f37c5106a6cc6
MinhTranPhuongGemi <106132946+MinhTranPhuongGemi@users.noreply.github.com>
2022-05-31 18:17:40 +0700

    add function changedvalue , animCompareCard

--- assets/Script/Entities/card.js
+++ assets/Script/Entities/card.js
@@ -1,7 +1,6 @@
 const Emitter = require("mEmitter");
 cc.Class({
     extends: cc.Component,
-
     properties: {
         contentCard: {
             default: null,
@@ -9,18 +8,25 @@ cc.Class({
         },
         stringCard: null,
     },
-
     // LIFE-CYCLE CALLBACKS:
-
     onLoad() {
+        Emitter.instance.registerEvent("changedValue", this.changedValue.bind(this));
     },
-
+    changedValue() {
+        if (this.contentCard.string != "") {
+            this.node.opacity = 255;
+        }
+        if (this.stringCard != "") {
+            this.contentCard.string = this.stringCard;
+            this.node.opacity = 255
+        }
+    },
+    animCompareEqual(){
+        let action = cc.sequence(cc.scaleTo(0.005, 1.2), cc.delayTime(0.01), cc.scaleTo(0.035, 1))
+        this.node.runAction(action);
+    },
     start() {
-
     },
-
     update(dt) {
-        //this.stringCard = this.contentCard.string;
     },
 });
-
