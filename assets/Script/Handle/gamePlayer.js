ac7c9a04f350778c4b66658947bbdeafbc309f63
nicotinne <106133099+nicotinne@users.noreply.github.com>
2022-06-01 19:14:33 +0700

    game player

--- assets/Script/Handle/gamePlayer.js
+++ assets/Script/Handle/gamePlayer.js
@@ -28,22 +28,24 @@ cc.Class({
     },
     onLoad() {
         this.rePlayGame();
+        this.registerEvent();
     },
-
     onEnable(){
         this.rePlayGame();
     },
-
     onDisable(){
         Emitter.instance.removeEvent("CLOSEGAMEPLAYER",this._closeGamePLayer);
         Emitter.instance.removeEvent("rePlayGame", this._rePlayGame);
-        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.handleKeyDown, this);
     },
-
+    registerEvent(){
+        Emitter.instance.registerEvent("moveUp",this.moveUp.bind(this));
+        Emitter.instance.registerEvent("moveDown",this.moveDown.bind(this));
+        Emitter.instance.registerEvent("moveLeft",this.moveLeft.bind(this));
+        Emitter.instance.registerEvent("moveRight",this.moveRight.bind(this));
+    },
     closeGamePlayer(){
         this.node.active = false;
     },
-
     rePlayGame(){
         this.scoreCurrent.getComponent(cc.Label).string = 0;
         this.gameBoard.removeAllChildren();
@@ -52,34 +54,16 @@ cc.Class({
         this._closeGamePLayer = this.closeGamePlayer.bind(this);
         Emitter.instance.registerEvent("CLOSEGAMEPLAYER",this._closeGamePLayer);
         Emitter.instance.registerEvent("rePlayGame", this._rePlayGame);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.handleKeyDown, this);
         this.createCard();
         this.randomCard();
     },
-
-    handleKeyDown(evt) {
+    handleClickInput() {
         if (this._canPress) return;
         this._canPress = true;
         Emitter.instance.emit("INPUT");
-        switch (evt.keyCode) {
-            case cc.macro.KEY.up:
-                this.moveUp();
-                break;
-            case cc.macro.KEY.down:
-                this.moveDown();
-                break;
-            case cc.macro.KEY.left:
-                this.moveLeft();
-                break;
-            case cc.macro.KEY.right:
-                this.moveRight();
-                break;
-            default:
-                this._canPress = false;
-                break;
-        }
     },
     moveUp() {
+        this.handleClickInput();
         for (let col = 0; col < 4; col++) {
             let flatArrCard = [0, 0, 0, 0];
             for (let row = 0; row < 4; row++) {
@@ -90,6 +74,7 @@ cc.Class({
         this.runAnimMoveCard();
     },
     moveDown() {
+        this.handleClickInput();
         for (let col = 0; col < 4; col++) {
             let flatArrCard = [0, 0, 0, 0];
             for (let row = 0; row < 4; row++) {
@@ -100,6 +85,7 @@ cc.Class({
         this.runAnimMoveCard();
     },
     moveLeft() {
+        this.handleClickInput();
         for (let row = 0; row < 4; row++) {
             let flatArrCard = [0, 0, 0, 0];
             for (let col = 0; col < 4; col++) {
@@ -110,6 +96,7 @@ cc.Class({
         this.runAnimMoveCard();
     },
     moveRight() {
+        this.handleClickInput();
         for (let row = 0; row < 4; row++) {
             let flatArrCard = [0, 0, 0, 0];
             for (let col = 0; col < 4; col++) {
@@ -137,181 +124,178 @@ cc.Class({
                             this._canPress = false;
                             this.randomCard();
                             this.randomCard(true);
-                        })
-                        .start();
-                    return;
+                        }).start();
+                        return;
+                    }
+                    this.arrAnim[i].start();
                 }
-                this.arrAnim[i].start();
             }
-        }
-    },
-    handleMove(arrCard) {
-        let hadMerge = [];
-        for (let i = 1; i < arrCard.length; i++) {
-            if (arrCard[i].getComponent("card").stringCard == "") {
-                continue;
-            }
-            let checkCompare = false;
-            let objAnim = { selfCard: null, otherCard: null, callBack: null }
-            for (let j = i - 1; j >= 0; j--) {
-                if (checkCompare == true) {
-                    j = -1;
-                    break;
+        },
+        handleMove(arrCard) {
+            let hadMerge = [];
+            for (let i = 1; i < arrCard.length; i++) {
+                if (arrCard[i].getComponent("card").stringCard == "") {
+                    continue;
                 }
-                checkCompare = this.changeValueCards(arrCard, i, j, objAnim, hadMerge);
+                let checkCompare = false;
+                let objAnim = { selfCard: null, otherCard: null, callBack: null }
+                for (let j = i - 1; j >= 0; j--) {
+                    if (checkCompare == true) {
+                        j = -1;
+                        break;
+                    }
+                    checkCompare = this.changeValueCards(arrCard, i, j, objAnim, hadMerge);
+                }
+                let cloneObjAnim = Object.assign(objAnim);
+                let animCard = this.defineAnimMoveCard(cloneObjAnim.selfCard, cloneObjAnim.otherCard, cloneObjAnim.callBack);
+                if (animCard != undefined) {
+                    this.arrAnim.push(animCard);
+                }
             }
-            let cloneObjAnim = Object.assign(objAnim);
-            let animCard = this.defineAnimMoveCard(cloneObjAnim.selfCard, cloneObjAnim.otherCard, cloneObjAnim.callBack);
-            if (animCard != undefined) {
-                this.arrAnim.push(animCard);
+        },
+        defineAnimMoveCard(selfCard, otherCard, callBack) {
+            if (selfCard != null && otherCard != null) {
+                let x = otherCard.x;
+                let y = otherCard.y;
+                let xOld = selfCard.x;
+                let yOld = selfCard.y;
+                return cc.tween(selfCard)
+                    .to(0.055, { position: cc.v2(x, y) }, { easing: "sineIn" })
+                    .to(0, { position: cc.v2(xOld, yOld) })
+                    .call(() => callBack(selfCard, otherCard))
+                    .call(() => { Emitter.instance.emit("changedValue") })
             }
-        }
-    },
-    defineAnimMoveCard(selfCard, otherCard, callBack) {
-        if (selfCard != null && otherCard != null) {
-            let x = otherCard.x;
-            let y = otherCard.y;
-            let xOld = selfCard.x;
-            let yOld = selfCard.y;
-            return cc.tween(selfCard)
-                .to(0.055, { position: cc.v2(x, y) }, { easing: "sineIn" })
-                .to(0, { position: cc.v2(xOld, yOld) })
-                .call(() => callBack(selfCard, otherCard))
-                .call(() => { Emitter.instance.emit("changedValue") })
-        }
-    },
-    changeValueCards(arrCard, i, j, objAnim, hadMerge) {
-        if (arrCard[j].getComponent("card").stringCard == "") {
-            if (j == 0) {
-                return this.compareNull(arrCard, i, j, objAnim);
+        },
+        changeValueCards(arrCard, i, j, objAnim, hadMerge) {
+            if (arrCard[j].getComponent("card").stringCard == "") {
+                if (j == 0) {
+                    return this.compareNull(arrCard, i, j, objAnim);
+                }
             }
-        }
-        else {
-            if (arrCard[j].getComponent("card").stringCard == arrCard[i].getComponent("card").stringCard) {
-                return this.compareEqual(arrCard, i, j, objAnim, hadMerge);
+            else {
+                if (arrCard[j].getComponent("card").stringCard == arrCard[i].getComponent("card").stringCard) {
+                    return this.compareEqual(arrCard, i, j, objAnim, hadMerge);
+                }
+                else if (arrCard[j].getComponent("card").stringCard != arrCard[i].getComponent("card").stringCard) {
+                    return this.compareDifferent(arrCard, i, j, objAnim);
+                }
             }
-            else if (arrCard[j].getComponent("card").stringCard != arrCard[i].getComponent("card").stringCard) {
-                return this.compareDifferent(arrCard, i, j, objAnim);
-            }
-        }
-    },
-    compareNull(arrCard, i, j, objAnim) {
-        let callBack = function (selfCard, otherCard) {
-            otherCard.getComponent("card").contentCard.string = otherCard.getComponent("card").stringCard;
-            selfCard.getComponent("card").contentCard.string = "";
-            selfCard.opacity = 0;
-            otherCard.opacity = 255;
-        }
-        objAnim.selfCard = arrCard[i];
-        objAnim.otherCard = arrCard[j];
-        objAnim.callBack = callBack;
-        objAnim.otherCard.getComponent("card").stringCard = objAnim.selfCard.getComponent("card").stringCard;
-        objAnim.selfCard.getComponent("card").stringCard = "";
-        return true;
-    },
-    compareEqual(arrCard, i, j, objAnim, hadMerge) {
-        if (hadMerge.indexOf(j) != -1) {
-            let oldJ = j + 1;
-            return this.compareNull(arrCard, i, oldJ, objAnim);
-        }
-        hadMerge.push(j);
-        let callBack = function (selfCard, otherCard) {
-            otherCard.getComponent("card").contentCard.string = otherCard.getComponent("card").stringCard;
-            selfCard.getComponent("card").contentCard.string = "";
-            otherCard.getComponent("card").animCompareEqual();
-            otherCard.opacity = 255;
-            selfCard.opacity = 0;
-        }
-        objAnim.selfCard = arrCard[i];
-        objAnim.otherCard = arrCard[j];
-        objAnim.callBack = callBack;
-        objAnim.otherCard.getComponent("card").stringCard = Number(objAnim.otherCard.getComponent("card").stringCard) * 2 + "";
-        objAnim.selfCard.getComponent("card").stringCard = "";
-        Emitter.instance.emit("updateScore",Number(objAnim.otherCard.getComponent("card").stringCard));
-        return true;
-    },
-    compareDifferent(arrCard, i, j, objAnim) {
-        let reValue = j + 1;
-        if (reValue != i) {
+        },
+        compareNull(arrCard, i, j, objAnim) {
             let callBack = function (selfCard, otherCard) {
                 otherCard.getComponent("card").contentCard.string = otherCard.getComponent("card").stringCard;
                 selfCard.getComponent("card").contentCard.string = "";
+                selfCard.opacity = 0;
+                otherCard.opacity = 255;
+            }
+            objAnim.selfCard = arrCard[i];
+            objAnim.otherCard = arrCard[j];
+            objAnim.callBack = callBack;
+            objAnim.otherCard.getComponent("card").stringCard = objAnim.selfCard.getComponent("card").stringCard;
+            objAnim.selfCard.getComponent("card").stringCard = "";
+            return true;
+        },
+        compareEqual(arrCard, i, j, objAnim, hadMerge) {
+            if (hadMerge.indexOf(j) != -1) {
+                let oldJ = j + 1;
+                return this.compareNull(arrCard, i, oldJ, objAnim);
+            }
+            hadMerge.push(j);
+            let callBack = function (selfCard, otherCard) {
+                otherCard.getComponent("card").contentCard.string = otherCard.getComponent("card").stringCard;
+                selfCard.getComponent("card").contentCard.string = "";
+                otherCard.getComponent("card").animCompareEqual();
                 otherCard.opacity = 255;
                 selfCard.opacity = 0;
             }
             objAnim.selfCard = arrCard[i];
-            objAnim.otherCard = arrCard[reValue];
+            objAnim.otherCard = arrCard[j];
             objAnim.callBack = callBack;
-            objAnim.otherCard.getComponent("card").stringCard = objAnim.selfCard.getComponent("card").stringCard;
+            objAnim.otherCard.getComponent("card").stringCard = Number(objAnim.otherCard.getComponent("card").stringCard) * 2 + "";
             objAnim.selfCard.getComponent("card").stringCard = "";
-        }
-        return true;
-    },
-
-    checkGameOver() {
-        for (let x = 0; x < 4; x++) {
-            for (let y = 0; y < 4; y++) {
-                if (x == 3) continue;
-                let self = this._arrBlocks[x][y].getComponent("card").stringCard;
-                let other = this._arrBlocks[x + 1][y].getComponent("card").stringCard;
-                if (self == other) {
-                    return true;
+            Emitter.instance.emit("updateScore",Number(objAnim.otherCard.getComponent("card").stringCard));
+            return true;
+        },
+        compareDifferent(arrCard, i, j, objAnim) {
+            let reValue = j + 1;
+            if (reValue != i) {
+                let callBack = function(selfCard, otherCard) {
+                    otherCard.getComponent("card").contentCard.string = otherCard.getComponent("card").stringCard;
+                    selfCard.getComponent("card").contentCard.string = "";
+                    otherCard.opacity = 255;
+                    selfCard.opacity = 0;
+                }
+                objAnim.selfCard = arrCard[i];
+                objAnim.otherCard = arrCard[reValue];
+                objAnim.callBack = callBack;
+                objAnim.otherCard.getComponent("card").stringCard = objAnim.selfCard.getComponent("card").stringCard;
+                objAnim.selfCard.getComponent("card").stringCard = "";
+            }
+            return true;
+        },
+        checkGameOver() {
+            for (let x = 0; x < 4; x++) {
+                for (let y = 0; y < 4; y++) {
+                    if (x == 3) continue;
+                    let self = this._arrBlocks[x][y].getComponent("card").stringCard;
+                    let other = this._arrBlocks[x + 1][y].getComponent("card").stringCard;
+                    if (self == other) {
+                        return true;
+                    }
                 }
             }
-        }
-        for (let x = 0; x < 4; x++) {
-            for (let y = 0; y < 4; y++) {
-                if (y == 3) continue;
-                let self = this._arrBlocks[x][y].getComponent("card").stringCard;
-                let other = this._arrBlocks[x][y + 1].getComponent("card").stringCard;
-                if (self == other) {
-                    return true;
+            for (let x = 0; x < 4; x++) {
+                for (let y = 0; y < 4; y++) {
+                    if (y == 3) continue;
+                    let self = this._arrBlocks[x][y].getComponent("card").stringCard;
+                    let other = this._arrBlocks[x][y + 1].getComponent("card").stringCard;
+                    if (self == other) {
+                        return true;
+                    }
                 }
             }
-        }
-        return false;
-    },
-
-    createCard() {
-        for (let col = 0; col < 4; col++) {
-            let arrRow = [];
-            for (let row = 0; row < 4; row++) {
-                let x = -225 + row * 150;
-                let y = 225 - col * 150;
-                let newCard = cc.instantiate(this.card);
-                newCard.parent = this.gameBoard
-                newCard.x = x;
-                newCard.y = y;
-                newCard.width = 140;
-                newCard.height = 140;
-                newCard.opacity = 0;
-                newCard.getComponent("card").stringCard = "";
-                cc.log(newCard.getComponent("card").contentCard.string)
-                arrRow.push(newCard);
+            return false;
+        },
+        createCard() {
+            for (let col = 0; col < 4; col++) {
+                let arrRow = [];
+                for (let row = 0; row < 4; row++) {
+                    let x = -225 + row * 150;
+                    let y = 225 - col * 150;
+                    let newCard = cc.instantiate(this.card);
+                    newCard.parent = this.gameBoard
+                    newCard.x = x;
+                    newCard.y = y;
+                    newCard.width = 140;
+                    newCard.height = 140;
+                    newCard.opacity = 0;
+                    newCard.getComponent("card").stringCard = "";
+                    cc.log(newCard.getComponent("card").contentCard.string)
+                    arrRow.push(newCard);
+                }
+                this._arrBlocks.push(arrRow)
             }
-            this._arrBlocks.push(arrRow)
-        }
-    },
-    randomCard(check = false) {
-        let flatArray = this._arrBlocks.flat(Infinity);
-        let arrNone = flatArray.filter((value) => {
-            return value.getComponent("card").stringCard == "";
-        })
-        if(arrNone.length == 0 ){
-            if (this.checkGameOver() == false) {
-                Emitter.instance.emit("GAMEOVER",Number(this.scoreCurrent.getComponent(cc.Label).string));
+        },
+        randomCard(check = false) {
+            let flatArray = this._arrBlocks.flat(Infinity);
+            let arrNone = flatArray.filter((value) => {
+                return value.getComponent("card").stringCard == "";
+            })
+            if(arrNone.length == 0 ){
+                if (this.checkGameOver() == false) {
+                    Emitter.instance.emit("GAMEOVER",Number(this.scoreCurrent.getComponent(cc.Label).string));
+                }
+                return;
             }
-            return;
-        }
-        if(check) return;
-        let index = Math.floor(Math.random() * arrNone.length)
-        let arrRandomNum = [2, 4];
-        let num = arrRandomNum[Math.floor(Math.random() * arrRandomNum.length)]
-        arrNone[index].getComponent("card").contentCard.string = num;
-        arrNone[index].getComponent("card").backgroundCard.color = COLOR[Number(num)];
-        arrNone[index].opacity = 255;
-        arrNone[index].getComponent("card").stringCard = num + ""
-        let action = cc.scaleTo(0.3, 1);
-        arrNone[index].runAction(action);
-    },
-});
+            if(check) return;
+            let index = Math.floor(Math.random() * arrNone.length)
+            let arrRandomNum = [2, 4];
+            let num = arrRandomNum[Math.floor(Math.random() * arrRandomNum.length)]
+            arrNone[index].getComponent("card").contentCard.string = num;
+            arrNone[index].getComponent("card").backgroundCard.color = COLOR[Number(num)];
+            arrNone[index].opacity = 255;
+            arrNone[index].getComponent("card").stringCard = num + ""
+            let action = cc.scaleTo(0.3, 1);
+            arrNone[index].runAction(action);
+        },
+    });
